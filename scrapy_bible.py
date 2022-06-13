7ed8f29e169dd72d7517fe3ff1e6f95f3ff3b2fd
Wesin Ribeiro <wesinec@gmail.com>
2022-06-13 12:05:13 -0300

    update scrapy bible

--- scrapy_bible.py
+++ scrapy_bible.py
@@ -7,50 +7,97 @@ Data: 6/04/2022
 """
 
 import requests, bs4
-from database import insert_book, insert_chapter, insert_verse
+from database import get_verse, insert_book, insert_chapter, insert_verseversion,\
+    insert_verse, get_all_versions, get_chapter, get_chapters_by_book, get_version
 
+"""Setup initial parameters."""
 url_base = 'https://www.bibliaonline.com.br'
 res = requests.get(url_base)
 soup = bs4.BeautifulSoup(res.text, 'html.parser')
-
-# get versions
 print("Iniciando a raspagem de dados!")
 
-# get books
-books = []
-books_link = soup.select('.jss50 .jss51 a')
-for index, book in enumerate(books_link):    
-    books.append({'index': index, 'name': book.getText(), 'abbr': book.attrs['href'].split('/')[-1]})
 
-# get chapters
-for book in books:
-    url = url_base + '/ara' + '/' + book['abbr']    
-    res = requests.get(url)
-    soup = bs4.BeautifulSoup(res.text, 'html.parser')
-    chapters = soup.select('.jss1 a')
-    book['number_chapters'] = len(chapters)        
-    book['id'] = insert_book(book['name'], book['abbr'], book['number_chapters'])    
+def get_versions():
+    """Get all versions."""
+    return get_all_versions()
 
 
-#print(books[0])
-# get verses
-number_verses = 0
-VERSION_ID = 1
-for book in books:
-    for i in range(book['number_chapters']):
-        url = url_base + '/ara' + '/' + book['abbr'] + '/' + str(i+1)
+def get_books():
+    """Get books."""
+    books = []
+    books_link = soup.select('.jss50 .jss51 a')
+    for index, book in enumerate(books_link):
+        books.append(
+            {
+                'index': index,
+                'name': book.getText(),
+                'abbr': book.attrs['href'].split('/')[-1]
+            }
+        )
+    return books
+
+
+def get_chapters(version, books):
+    """Get chapters."""
+    for book in books:
+        url = f"{url_base}/{version[2]}/{book['abbr']}"
         res = requests.get(url)
         soup = bs4.BeautifulSoup(res.text, 'html.parser')
-        verses = soup.select('p')
-        number_verses += len(verses)
-        chapter_id = insert_chapter(book['id'], i+1, str(len(verses)))
-        for index, verse in enumerate(verses):
-            if verse.attrs['class'][0] == 'MuiTypography-root':
-                break
-            verse_number = len(str(index + 1))
-            verse_text = verse.getText()[verse_number:].strip()
-            verse_id = insert_verse(book['id'], chapter_id, VERSION_ID, index+1, verse_text)
-            print(f"{book['name']} cap {i+1} v. {index + 1} cadastrado!")
-        
+        chapters = soup.select('.jss1 a')
+        book['number_chapters'] = len(chapters)
+        book['id'] = insert_book(
+            book['name'], book['abbr'],
+            book['number_chapters']
+        )
+    return books
 
-print("Terminado a raspagem de dados!")
+
+def get_verses(version, books):
+    """Get verses."""
+    number_verses = 0
+    for book in books:
+        for i in range(book['number_chapters']):
+            url = f"{url_base}/\
+                {version[2]}/{book['abbr']}/{str(i+1)}"
+            res = requests.get(url)
+            soup = bs4.BeautifulSoup(res.text, 'html.parser')
+            verses = soup.select('p')
+            number_verses += len(verses)
+            chapter = get_chapter(book['id'], i+1)
+            if chapter is None:
+                chapter_id = insert_chapter(book['id'], i+1, str(len(verses)))
+            else:
+                chapter_id = chapter[0]
+            
+            for index, verse in enumerate(verses):
+                if verse.attrs['class'][0] == 'MuiTypography-root':
+                    break
+                verse_number = len(str(index + 1))
+                verse_text = verse.getText()[verse_number:].strip().replace(
+                    "'", "''")
+                current_verse = get_verse(book['id'], chapter_id, index+1)
+                if current_verse is None:
+                    verse_id = insert_verse(book['id'], chapter_id, index+1)
+                    insert_verseversion(verse_id, version[0], verse_text)
+                else:
+                    insert_verseversion(
+                        current_verse[0],
+                        version[0],
+                        verse_text
+                    )
+                
+                print(f"{book['name']} cap {i+1} v. {index + 1} cadastrado!")
+
+
+if __name__ == '__main__':
+    # versions = get_versions()
+    # books = get_books()    
+    # books = get_chapters_by_book(versions[0], books)
+    # for version in versions:
+    #     get_verses(version, books)
+
+    version = get_version(4)
+    books = get_books()
+    get_verses(version, books)
+
+    print("Terminado a raspagem de dados!")
